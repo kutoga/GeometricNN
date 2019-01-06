f41006b3d2894df9a4fbe14e95942c0a0c55609d
Benjamin B. Meier <benjamin.meier70@gmail.com>
2019-01-06 21:30:41 +0100

    Fixed nn backward things

--- mult_nn/_nn_ops.py
+++ mult_nn/_nn_ops.py
@@ -1,7 +1,8 @@
 from typing import Callable
-from functools import lru_cache
 
 import numpy as np
+
+from sympy import Expr
 
 from ._math import MExpression, MSymbol, dydx, uyux
 
@@ -9,6 +10,9 @@ DerivativeOperator = Callable[[MExpression, MSymbol], MExpression]
 ChainRuleOperator = Callable[[np.ndarray, np.ndarray, MExpression, MSymbol], np.ndarray]
 
 class DerivativeRule:
+    __DX_RULE = None
+    __UX_RULE = None
+
     def __init__(self, derivative_op: DerivativeOperator, chain_rule_op: ChainRuleOperator) -> None:
         self.__derivative_op = derivative_op
         self.__chain_rule_op = chain_rule_op
@@ -20,18 +24,33 @@ class DerivativeRule:
         return self.__chain_rule_op(derivative_g_x, g_x, f, x)
 
     @staticmethod
-    @lru_cache(maxsize=1)
-    def dx_rule() -> 'DerivativeRule':
+    def _try_numpify(x: Expr) -> Expr:
+        if isinstance(x, np.ndarray):
+            return x.astype(float)
+        return x
+
+    @staticmethod
+    def __dx_rule() -> 'DerivativeRule':
         def dx_chain(derivative_g_x: np.ndarray, g_x: np.ndarray, f: MExpression, x: MSymbol) -> np.ndarray:
-            return dydx(f, x)((x, g_x)).sympy() * derivative_g_x
+            return DerivativeRule._try_numpify(dydx(f, x)((x, g_x)).sympy() * derivative_g_x)
         return DerivativeRule(dydx, dx_chain)
 
     @staticmethod
-    @lru_cache(maxsize=1)
-    def ux_rule() -> 'DerivativeRule':
+    def __ux_rule() -> 'DerivativeRule':
         def ux_chain(derivative_g_x: np.ndarray, g_x: np.ndarray, f: MExpression, x: MSymbol) -> np.ndarray:
-            return uyux(f, x)((x, g_x)).sympy() ** (g_x * np.log(derivative_g_x))
+            return DerivativeRule._try_numpify(uyux(f, x)((x, g_x)).sympy() ** (g_x * np.log(derivative_g_x)))
         return DerivativeRule(uyux, ux_chain)
 
+    @staticmethod
+    def dx_rule() -> 'DerivativeRule':
+        if DerivativeRule.__DX_RULE is None:
+            DerivativeRule.__DX_RULE = DerivativeRule.__dx_rule()
+        return DerivativeRule.__DX_RULE
+
+    @staticmethod
+    def ux_rule() -> 'DerivativeRule':
+        if DerivativeRule.__UX_RULE is None:
+            DerivativeRule.__UX_RULE = DerivativeRule.__ux_rule()
+        return DerivativeRule.__UX_RULE
 
 
--- test/test_nn.py
+++ test/test_nn.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Callable
 import pytest
 import numpy as np
 
@@ -6,7 +6,7 @@ from sympy import exp
 
 from mult_nn._math import MExpression, sym, maximum
 from mult_nn._nn_ops import DerivativeRule
-from mult_nn._nn import Layer, ActivationLayer, LinearLayer, LayerCollection
+from mult_nn._nn import Layer, ActivationLayer, WeightsGenerator, LinearLayer, LayerCollection
 
 x = sym('x')
 
@@ -51,17 +51,17 @@ def test_layer_collection_activation_forward(layers: List[Layer], input: np.ndar
     np.testing.assert_almost_equal(output, expected_output)
 
 @pytest.mark.parametrize('layers, input, previous, derivative_rule, expected_output', [
+    #([
+    #    ActivationLayer(MExpression(x ** 2 + x)),
+    #    ActivationLayer(MExpression(-x)),
+    #    ActivationLayer(MExpression(exp(x)))
+    # ], np.array([0., 1.]), np.array([1., 1.]), DerivativeRule.dx_rule(), np.array([-1., -3. * np.exp(-2.)])),
     ([
-        ActivationLayer(MExpression(x ** 2 + x)),
-        ActivationLayer(MExpression(-x)),
-        ActivationLayer(MExpression(exp(x)))
-     ], np.array([0., 1.]), np.array([1., 1.]), DerivativeRule.dx_rule(), np.array([-1., -3. * np.exp(-2.)])),
-    ([
-        ActivationLayer(MExpression(x ** 2)),
+        ActivationLayer(MExpression(x ** 2 + 1)),
         ActivationLayer(MExpression(exp(x))),
         ActivationLayer(MExpression(x * 13.)),
         ActivationLayer(MExpression(x + 1))
-     ], np.array([0., 1.]), np.array([1., 1.]), DerivativeRule.ux_rule(), np.array([1., np.exp((26. * np.exp(1.)) / (13 * np.exp(1.) + 1))]))
+     ], np.array([0., 1.]), np.array([np.exp(1.), np.exp(1.)]), DerivativeRule.ux_rule(), np.array([1., np.exp((26. * np.exp(2.)) / (13 * np.exp(2.) + 1))]))
 ])
 def test_layer_collection_activation_backward(layers: List[Layer], input: np.ndarray, previous: np.ndarray,
                                               derivative_rule: DerivativeRule, expected_output: np.ndarray) -> None:
@@ -71,11 +71,18 @@ def test_layer_collection_activation_backward(layers: List[Layer], input: np.nda
 
     np.testing.assert_almost_equal(backward_output, expected_output)
 
-# @pytest.mark.parametrize('layer, input, expected_output', [
-#     (LinearLayer(3, 2, bias=True, weights=np.array([[1., 2., -1.], [3., 4., -2.]]), bias_weights=np.array([5., 6.])),
-#      np.array([[2., 3., -1.]]), np.array([[14., 26.]]))
-# ])
-# def test_linear_activation(layer: LinearLayer, input: np.ndarray, expected_output: np.ndarray) -> None:
-#     output = layer.forward(input)
-#
-#     np.testing.assert_almost_equal(output, expected_output)
+@pytest.mark.parametrize('layer_builder, weights, bias, input, expected_output', [
+     ((lambda weights_gen, bias_gen: LinearLayer(3, 2, True, weights_gen, bias_gen)),
+      np.array([[1., 3.], [2., 4.], [-1., -2.]]), np.array([5., 6.]),
+      np.array([[2., 3., -1.]]), np.array([[14., 26.]])),
+    ((lambda weights_gen, bias_genn: LinearLayer(2, 1, True, weights_gen, bias_genn)),
+     np.array([[1.], [2.]]), np.array([-1.]),
+     np.array([[4., 5.], [2., 3.]]), np.array([[13.], [7.]]))
+])
+def test_linear_activation(layer_builder: Callable[[WeightsGenerator, WeightsGenerator], LinearLayer],
+                           weights: np.ndarray, bias: np.ndarray, input: np.ndarray,
+                           expected_output: np.ndarray) -> None:
+    layer = layer_builder(lambda _: weights, lambda _: bias)
+    output = layer.forward(input)
+
+    np.testing.assert_almost_equal(output, expected_output)
