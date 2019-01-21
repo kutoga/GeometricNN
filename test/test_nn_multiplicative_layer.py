d6b338372bbca73de60047ccb89b0342f29cd30c
Benjamin B. Meier <benjamin.meier70@gmail.com>
2019-01-21 22:43:56 +0100

    Testing...

--- test/test_nn_multiplicative_layer.py
+++ test/test_nn_multiplicative_layer.py
@@ -28,14 +28,10 @@ def test_non_positive_weights_should_not_be_allowed(weights: np.ndarray, bias: n
         MultiplicativeLayer(weights.shape[1], weights.shape[0], weights_generator=lambda _: weights,
                             bias=bias is not None, bias_generator=lambda _: bias)
 
-@pytest.mark.parametrize('bias', [
-    np.array([1.]), np.array([3.]), None
-])
-@pytest.mark.parametrize('weights, input, previous, expected_output', [
-    (np.array([[1., 2., 3.]]), np.array([1., 2., 1.]), np.array([1.]), np.array([1., 1., 1.])),
-    (np.array([[1., 2., 3.]]), np.array([1., 2., 1.]), np.array([np.exp(1.)]), np.array([1., 2.**12., 3.**12.])),
-    #(np.array([[1., -1., 2.]]), np.array([2., -1., 3.]), np.array([1.]), np.array([1., -1., 2.])),
-    #(np.array([[0., -3., 22.]]), np.array([12., -1111., -13.]), np.array([-1.]), np.array([0., 3., -22.])),
+@pytest.mark.parametrize('weights, bias, input, previous, expected_output', [
+    (np.array([[1., 2., 3.]]), np.array([5.]), np.array([1., 2., 1.]), np.array([1.]), np.array([1., 1., 1.])),
+    (np.array([[1., 2., 3.]]), np.array([1.]), np.array([1., 2., 1.]), np.array([np.exp(1.)]), np.array([1., 2.**12., 3.**12.])),
+    (np.array([[1., 2., 3.]]), np.array([2.]), np.array([1., 2., 1.]), np.array([np.exp(1.)]), np.array([1., 2.**24., 3.**24.])),
 ])
 def test_multiplicative_activation_backprop_with_single_output(weights: np.ndarray, bias: np.ndarray, input: np.ndarray,
                                     previous: np.ndarray, expected_output: np.ndarray) -> None:
@@ -47,7 +43,7 @@ def test_multiplicative_activation_backprop_with_single_output(weights: np.ndarr
 
     np.testing.assert_almost_equal(backward_output, expected_output)
 
-"""
+
 @pytest.mark.parametrize('weights, bias, input, previous, expected_output', [
     (np.array([[2., 3., -1.], [0., -0.5, 1.]]), np.array([1., 2.]), np.array([1., 2., -1.]),
      np.array([1., 1.]), np.array([2., 2.5, 0.])),
@@ -64,6 +60,7 @@ def test_multiplicative_activation_backprop_with_multiple_outputs(weights: np.nd
 
     np.testing.assert_almost_equal(backward_output, expected_output)
 
+"""
 @pytest.mark.parametrize('weights, bias, input, previous, expected_updated_weights, expected_updated_bias', [
     (np.array([[1., 2.], [3., 4.]]), np.array([-1., 2.]), np.array([3., 4.]), np.array([2., -2.]),
      np.array([[-5, -6.], [9., 12.]]), np.array([-3., 4.]))
